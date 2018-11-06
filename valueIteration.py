5f95d90b35866276724b7c47fcdd4f00be4e4963
dylanmeehan <dylanmeehan@gmail.com>
2018-11-05 20:48:03 -0500

    value iteration seems to work!

--- valueIteration.py
+++ valueIteration.py
@@ -20,17 +20,41 @@ class ValueIteration(TableBased):
     if (abs(phi) > np.pi/4):
       return 0
     else:
-      return 1
+      return (1-phi)**2
+
+
+  # take in a (continuous) state
+  def calc_best_action_and_utility(self, state):
+
+    Qtemp = np.zeros(self.num_actions)
+
+    for action_index in range(self.num_actions):
+      action = self.get_action_from_index(action_index)
+      (new_state, _, _) = self.step(state, action)
+
+      new_state3 = state_to_state3(new_state)
+      Qtemp[action_index] = self.get_value(new_state3)
+
+   # print(Qtemp)
+
+    max_Qtemp = np.max(Qtemp)
+    best_action_index = np.argmax(Qtemp)
+
+    return (best_action_index, max_Qtemp)
 
   # called by simulate state.
   # given state_grid_point_index (a discritized state 3 tuple).
   # return the index of the action to take
   def act_index(self, state_grid_point_index, epsilon):
-    print("utilities: " + str(self.U[state_grid_point_index]))
-    print("action: " + str(np.argmax(self.U[state_grid_point_index])))
-    return np.argmax(self.U[state_grid_point_index])
 
-  # return a value for the (continous) new state
+    state3 = self.state_grid_points[state_grid_point_index]
+    state = state3_to_state(state3)
+
+    (best_action_index, _) = self.calc_best_action_and_utility(state)
+    print(best_action_index)
+    return best_action_index
+
+  # return a value for the (continous) new_state3
   #interpolater
   def get_value(self,new_state3):
     #interpolate
@@ -49,41 +73,31 @@ class ValueIteration(TableBased):
 
     while (n_episode < num_episodes):
 
-      #self.itp = RegularGridInterpolator(\
-      #  (self.phi_grid, self.phi_dot_grid, self.delta_grid),self.U)
-      count = 0
+      self.itp = RegularGridInterpolator(\
+        (self.phi_grid, self.phi_dot_grid, self.delta_grid),self.U)
+
       #exhaustively loop through all states
       for phi_index in range(self.len_phi_grid):
         for phi_dot_index in range(self.len_phi_dot_grid):
           for delta_index in range (self.len_delta_grid):
 
-            Qtemp = np.zeros(self.num_actions)
-
             state3_index = (phi_index, phi_dot_index, delta_index)
             state3 = self.state_grid_points[state3_index]
             state = state3_to_state(state3)
 
-            for action_index in range(self.num_actions):
-              action = self.get_action_from_index(action_index)
-              (new_state, _, _) = self.step(state, action)
-
-              new_state3 = self.state_grid_points[state3_index]
-              Qtemp[action_index] = gamma*self.get_value(new_state3)
-
-            print(Qtemp)
+            (_, max_Qtemp) = self.calc_best_action_and_utility(state)
 
 
-            count += 1
             #If the bike fell down, set the reward to 0
             if (self.get_reward(state) == 0):
               self.U[state3_index] = 0
             else:
-              self.U[state3_index] = self.get_reward(state) + np.max(Qtemp)
+              self.U[state3_index] = self.get_reward(state) + gamma*max_Qtemp
 
 
 
       n_episode += 1
-      print('Epsiode: ' + str(n_episode) + "count: " + str(count))
+      print('Epsiode: ' + str(n_episode))
 
     print("done trianing")
     print(self.U)
@@ -107,7 +121,7 @@ class ValueIteration(TableBased):
 
 
 VIteration_model = ValueIteration()
-VIteration_model.train()
+#VIteration_model.train()
 
 VIteration_model.test(Ufile = "valueIteration_U.csv", tmax = 10, state_flag = 0,
       gamma = 1)
