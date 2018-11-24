c495ff7e4ee34cacc240e6809a4f535b9823720f
dylanmeehan <dylanmeehan@gmail.com>
2018-11-24 18:23:49 -0500

    tested value iteration with discrete actions, failure

--- main.py
+++ main.py
@@ -13,22 +13,24 @@ figObject = None
 #   gamma =1, figObject = figObject)
 
 
-VIteration_model = ValueIteration(state_grid_flag = 0, action_grid_flag = 2,
- reward_flag = 1, Ufile = "VI8.csv", use_only_continuous_actions = False)
-#VIteration_model.train(num_episodes = 10,
-#     do_interpolation = True, use_continuous_actions = True)
-# figObject = VIteration_model.test(tmax = 10, state_flag = state_flag,
-#   use_continuous_actions = True, gamma = 3, figObject = figObject)
+VIteration_model = ValueIteration(state_grid_flag = 0, action_grid_flag = 3,
+ reward_flag = 3, Ufile = "VI11.csv", use_only_continuous_actions = False)
+VIteration_model.train(num_episodes = 10,
+     do_interpolation = True, use_continuous_actions = False)
+figObject = VIteration_model.test(tmax = 10, state_flag = state_flag,
+   use_continuous_actions = True, gamma = 1, figObject = figObject)
+figObject = VIteration_model.test(tmax = 10, state_flag = state_flag,
+   use_continuous_actions = False, gamma = 1, figObject = figObject)
 #VIteration_model.heatmap_value_function()
-VIteration_model.heatmap_of_policy(option = "average", include_linear_controller = True,
-  use_continuous_actions = False)
+#VIteration_model.heatmap_of_policy(option = "zero", include_linear_controller = True,
+#  use_continuous_actions = )
 
 
 #I don't need to store the figObject returned by test. this returns the same
 # figObject as before. We only get a new FigObject when we initialize figObject
 # to None
-#figObject = runBicycleTest(state_flag, controller = LinearController.LinearController(),
-#  time = 10, isGraphing  = True, figObject = figObject)
+figObject = runBicycleTest(state_flag, controller = LinearController.LinearController(),
+ time = 10, isGraphing  = True, figObject = figObject)
 
 
 plt.show()
